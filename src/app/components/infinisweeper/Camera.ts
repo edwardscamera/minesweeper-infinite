7e7fff0e61f6af8bc7407d8df79af94bb32bbd31
edwardscamera <42257904+edscamera@users.noreply.github.com>
2024-08-11 15:26:26 -0400

    Round camera pos on zoom

--- src/app/components/infinisweeper/Camera.ts
+++ src/app/components/infinisweeper/Camera.ts
@@ -11,8 +11,13 @@ export class Camera {
 
     public zoom(zoom: number) {
         this.ppu *= 0.9 ** zoom;
-
         this.ppu = Math.max(16, Math.min(256, this.ppu));
+        this.roundPositionToNearestPixel();
+    }
+
+    private roundPositionToNearestPixel(): void {
+        this.position.x = Math.round(this.position.x * this.ppu) / this.ppu;
+        this.position.y = Math.round(this.position.y * this.ppu) / this.ppu;
     }
 }
 
